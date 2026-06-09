c13287f1bdf17c7cdee5833d9cc4e0240a573e62
NANASKAAAA <z7gdrp6b2z@privaterelay.appleid.com>
2026-06-09 19:19:36 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Cherī community ",
+        name: "Cherī community",
         // Activity type number (0 = Playing).
         type: 2, 
       },
